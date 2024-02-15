3dc72c67310e57a9d6899e783297e0d87dfcd23f
jocile <jocile@gmail.com>
2024-02-14 21:29:39 -0300

    🎨 Estilizando o botão sair da navbar

---
+++ frontweb/src/components/ButtonLogout/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const ButtonLogout = () => {
+    return (
+        <div className="nav-logout">
+            <h6>SAIR</h6>
+        </div>
+    )
+}
+
+export default ButtonLogout;
--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,21 +1,22 @@
 import { Link } from 'react-router-dom';
+import ButtonLogout from 'components/ButtonLogout';
+
 import './styles.css';
+
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-warning main-nav">
       <div className="container-fluid">
         <div>
-        <Link to="/" className="nav-logo-text">
+          <Link to="/" className="nav-logo-text">
             <h1>MovieFlix</h1>
           </Link>
         </div>
         <div>
-          <ul>
-            <li>
-              <a href="link">SAIR</a>
-            </li>
-          </ul>
+          <a href="#logout">
+            <ButtonLogout />
+          </a>
         </div>
       </div>
     </nav>
