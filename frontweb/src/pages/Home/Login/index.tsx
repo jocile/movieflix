b78af68dcce1fbf327947280936c025f2c7591aa
jocile <jocile@gmail.com>
2024-02-15 21:19:52 -0300

    ✨ Gerenciamento manual de erros de login

--- frontweb/src/pages/Home/Login/index.tsx
+++ frontweb/src/pages/Home/Login/index.tsx
@@ -1,6 +1,7 @@
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
+import { useState } from 'react';
 
 import './styles.css';
 
@@ -10,6 +11,7 @@ type FormData = {
 };
 
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
 
   const {
     register,
@@ -19,9 +21,11 @@ const Login = () => {
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
-        console.log('SUCESSO', response);        
+        setHasError(false);
+        console.log('SUCESSO', response);
       })
-      .catch((error) => {        
+      .catch((error) => {
+        setHasError(true);
         console.log('ERRO', error);
       });
   };
@@ -29,6 +33,9 @@ const Login = () => {
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
