8e95439cff77c9b7ac29cd6102e8b691a2a0c963
jocile <jocile@gmail.com>
2024-03-03 19:32:08 -0300

    ✨ Card de catálogo de filmes adicionado

--- frontweb/src/pages/Private/MovieCatalog/index.tsx
+++ frontweb/src/pages/Private/MovieCatalog/index.tsx
@@ -1,5 +1,8 @@
 import { AxiosRequestConfig } from 'axios';
-import { useEffect, useState } from 'react';
+import MovieCatalogCard from 'components/MovieCatalogCard';
+import MovieFilter, { MovieFilterData } from 'components/MovieFilter';
+import Pagination from 'components/Pagination';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { SpringPage } from 'types/vendor/spring';
@@ -7,35 +10,68 @@ import { requestBackend } from 'util/requests';
 
 import './styles.css';
 
+type ControlComponentsData = {
+  activePage: number;
+  filterData : MovieFilterData;
+}
+
 const MovieCatalog = () => {
   const [page, setPage] = useState<SpringPage<Movie>>();
 
-  useEffect(() => {
-    const params: AxiosRequestConfig = {
+  const [controlComponentsData, setControlComponentsData] = useState<ControlComponentsData>(
+    {
+      activePage: 0,
+      filterData: { genre: null },
+    }
+  );
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({activePage: pageNumber, filterData: controlComponentsData.filterData});
+  };
+
+  const handleSubmitFilter = (data : MovieFilterData) => {
+    setControlComponentsData({activePage: 0, filterData: data});
+  }
+
+  const getMovies = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
       url: '/movies',
-      withCredentials: true,
       params: {
-        page: 0,
-        size: 12,
+        page: controlComponentsData.activePage,
+        size: 4,
+        genreId: controlComponentsData.filterData.genre?.id
       },
+      withCredentials: true,
     };
 
-    requestBackend(params).then((response) => {
+    requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  }, []);
+  }, [controlComponentsData])
+
+    useEffect(() => {
+      getMovies();
+    }, [getMovies]);    
 
   return (
-    <div className="catalog-container">
-      <h1>Tela listagem de filmes</h1>
-      <div>
-        <Link to="/movies/1">
-           <p>Acessar /movies/1</p>
-        </Link>
-        <Link to="/movies/2">
-           <p>Acessar /movies/2</p>
-        </Link>
+    <div className="container my-4 movie-catalog-container">
+      <MovieFilter onSubmitFilter={handleSubmitFilter} />
+      <div className="row">
+        {page?.content.map((movie) => (
+          <div key={movie.id} className="col-sm-6 col-xl-3">
+            <Link to={`/movies/${movie.id}`}>
+              <MovieCatalogCard movies={movie} />
+            </Link>
+          </div>
+        ))}
       </div>
+      <Pagination 
+        forcePage={page?.number}
+        pageCount={page ? page?.totalPages : 0} 
+        range={3} 
+        onChange={handlePageChange} 
+      />
     </div>
   );
 };
